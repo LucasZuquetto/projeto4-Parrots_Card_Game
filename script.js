6ebbbe4d106c40d71e5ebc84dd6daa420901e0fe
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2022-06-19 18:14:15 -0300

    Gerando as cartas com Javascript

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ while (isNaN(QuantidadeCartas) || QuantidadeCartas % 2 == 1 || 4 > QuantidadeCar
     QuantidadeCartas = prompt('Número inválido, digite novamente a quantidade de cartas que você deseja jogar')
     QuantidadeCartas = Number(QuantidadeCartas)
 }
-console.log(QuantidadeCartas)
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+const ParrotName = ['bobross','explody','fiesta', 'metal', 'revertit', 'triplets','unicorn']
+ParrotName.sort(comparador)
+
+for( let i=0 ; i < QuantidadeCartas ; i += 2){
+    const CardTemplate = `
+    <div class="card">
+        <img class="imgfront" src="./img/${ParrotName[i]}parrot.gif">
+    </div>
+    `
+    document.querySelector('.content').innerHTML += CardTemplate + CardTemplate
+    
+}
