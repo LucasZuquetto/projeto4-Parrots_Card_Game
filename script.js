c3aeba99ca86df8395dc42c163717f71e9d0e7ef
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2022-06-20 12:13:57 -0300

    array com todas cartas em ordem aleatória pronto

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
+let Deck = []
+const DeckPar2 = []
+const ParrotName = ['bobross','explody','fiesta', 'metal', 'revertit', 'triplets','unicorn']
+
+//validação do prompt
 let QuantidadeCartas = prompt('Com quantas cartas você deseja jogar?')
 QuantidadeCartas = Number(QuantidadeCartas)
 while (isNaN(QuantidadeCartas) || QuantidadeCartas % 2 == 1 || 4 > QuantidadeCartas || QuantidadeCartas > 14){
     QuantidadeCartas = prompt('Número inválido, digite novamente a quantidade de cartas que você deseja jogar')
     QuantidadeCartas = Number(QuantidadeCartas)
 }
-
+// função número aleatório
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+
+QuantidadeCartas = QuantidadeCartas/2
+//construindo uma array com todas cartas em ordem aleatória
+for( let i=0 ; i < QuantidadeCartas ; i ++){
+    Deck.push(ParrotName[i])
+}
+for( let i=0 ; i < QuantidadeCartas ; i ++){
+    DeckPar2.push(ParrotName[i])
 }
 
+Deck.push.apply(Deck, DeckPar2)
+Deck.sort(comparador)
 
-for( let i=0 ; i < QuantidadeCartas ; i ++){
-    const ParrotName = ['bobross','explody','fiesta', 'metal', 'revertit', 'triplets','unicorn']
-    ParrotName.sort(comparador)
-
+//colocando todas cartas no DOM
+for( let i=0 ; i < QuantidadeCartas*2 ; i ++){
     const CardTemplate = `
     <div class="card">
         <img class="face front-face" src="./img/front.png">
-        <img class="face back-face" src="./img/${ParrotName[getRandomIntInclusive(0, 6)]}parrot.gif">
+        <img class="face back-face" src="./img/${Deck[i]}parrot.gif">
     </div>
     `
     document.querySelector('.content').innerHTML += CardTemplate
 }
 
-
-const card = document.querySelectorAll('.card');
+//função para a carta girar com um clique
+const flipcard = document.querySelectorAll('.card');
 function flipCard() {
   this.classList.toggle('flip');
 }
-card.forEach(card => card.addEventListener('click', flipCard));
+flipcard.forEach(card => card.addEventListener('click', flipCard));
+
+//
